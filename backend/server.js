b99738911ed9ab67004403152c62806567aac91e
Piyush Verma <piyushverma.6927@gmail.com>
2026-06-13 02:09:34 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
+
+import cors from "cors";
+app.use(cors());
+
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
 
 
 const express = require("express");
