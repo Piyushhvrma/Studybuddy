2aef154b989c2efc9d6547121fc021f635649fe0
Piyush Verma <piyushverma.6927@gmail.com>
2026-06-13 16:03:51 +0530

    fix: clean up duplicated server.js content

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,5 @@
-
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running"));
-
 
 const express = require("express");
 const mongoose = require("mongoose");
